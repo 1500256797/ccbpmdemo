4489486bd04b0c9b303703142c900f3bb880e0c8
ZhenQian Zhang <zhangzhenqian1996@gmail.com>
2020-02-17 18:28:59 +0800

    测试接口，创建工作流实例接口待完成

--- src/main/java/com/example/jflowdemo/controller/RestfullController.java
+++ src/main/java/com/example/jflowdemo/controller/RestfullController.java
@@ -35,15 +35,12 @@ public class RestfullController {
         return "success" ;
     }
 
+    //待办
     @RequestMapping(value = "/DB_Todolist")
     public String DbToDoList(){
-//        String userNo = "zhoutianjiao";
-//        String sysNo = " ";
-//        System.out.println("userNo = "+userNo);
-//        String tempList = jflowService.DB_Todolist(userNo, sysNo);
-        //String tempList = jflowService.DbToDoList();
-        //return tempList;
-        return " ";
+        //sysno应填空字符串，任何空格形式的字符串被禁止
+        String result = jflowService.DB_Todolist("zhoutianjiao","");
+        return result;
     }
 
     //可以发起的流程
@@ -79,5 +76,76 @@ public class RestfullController {
 //        System.out.println("打印请求投："+request.getHeader("Cookie"));
 //        System.out.println("请求参数："+request.getParameter("DoType"));
 //        jflowService.ProcessRequest(request);
+//    }
+
+
+    /**
+     * 我发起的流程实例
+     * @param userNo 用户编号
+     * @param domain 统编号,为空时返回平台所有数据
+     * @param pageSize
+     * @param pageIdx
+     * @return
+     */
+    @RequestMapping(value = "/DB_MyStartFlowInstance")
+    public String DB_MyStartFlowInstance(@RequestParam("userNo")  String userNo, @RequestParam("domain")  String domain, @RequestParam("pageSize")  int pageSize, @RequestParam("pageIdx") int pageIdx){
+        String tempResult = jflowService.DB_MyStartFlowInstance(userNo,domain,pageSize,pageIdx);
+        return tempResult;
+    }
+//    {
+//        "WorkID": 290,
+//            "StarterName": "周天娇",
+//            "Title": "集团研发部-zhoutianjiao,周天娇在2019-11-16 11:25发起.",
+//            "WFSta": 0,
+//            "WFState": 2,
+//            "NodeName": "节点2",
+//            "RDT": "2019-11-16 11:26",
+//            "BillNo": "",
+//            "FlowNote": "",
+//            "FK_FlowSort": "100",
+//            "FK_Flow": "015",
+//            "FK_Dept": "1002",
+//            "FID": 0,
+//            "FK_Node": 1502,
+//            "FK_NY": "2019-11",
+//            "MyNum": 0,
+//            "FlowName": "pop返回值demo",
+//            "Starter": "zhoutianjiao",
+//            "Sender": "zhoutianjiao",
+//            "DeptName": "集团研发部",
+//            "PRI": 1,
+//            "SDTOfNode": "2019-11-17 11:26",
+//            "SDTOfFlow": "",
+//            "PFlowNo": "",
+//            "PWorkID": 0,
+//            "PNodeID": 0,
+//            "PFID": 0,
+//            "PEmp": "",
+//            "CFlowNo": "",
+//            "CWorkID": 0,
+//            "GuestNo": "",
+//            "GuestName": "",
+//            "TodoEmps": "zhoupeng,周朋;",
+//            "TodoEmpsNum": 1,
+//            "TaskSta": 0,
+//            "AtPara": "@CH1502=2019-11-17 11:26@LastTruckID=2123207970",
+//            "Emps": "@zhoutianjiao@",
+//            "GUID": "",
+//            "SysType": "",
+//            "SendDT": "2019-11-16 11:26",
+//            "WeekNum": 0,
+//            "TSpan": 0,
+//            "Domain": "",
+//            "TodoSta": 0,
+//            "PrjNo": "",
+//            "PrjName": "",
+//            "SDTOfFlowWarning": "",
+//            "DoDomain": ""
+//    }
+    //发起一个流程
+//    public String createWork(){
+//
+//        jflowService.
+//        return " ";
 //    }
 }
